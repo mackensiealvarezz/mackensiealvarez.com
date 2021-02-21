1072694283198a00c693c0eeef0ce022de60250d
Mackensie Alvarez <mackensiealvarez@gmail.com>
2021-02-20 22:00:07 -0500

    resources/views/app.blade: adding tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,8 +5,16 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     <meta name="url" content="{{ url('') }}" />
+    <meta name="twitter:card" content="summary">
+    </meta>
+    <meta name="twitter:image" content="{{ asset('images/me.jpeg') }}">
+    </meta>
+    <meta name="description" content="Mackensie Alvarez | I create things">
+    <meta name="twitter:title" content="Mackensie Alvarez">
+    <meta name="twitter:description" content="Fullstack developer">
     <link href="{{ mix('/css/app.css') }}" rel="stylesheet" />
     <script src="{{ mix('/js/app.js') }}" defer></script>
+    <title>Mackensie Alvarez | I create things</title>
 </head>
 
 <body>
